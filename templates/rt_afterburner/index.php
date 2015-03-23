837bcaf77499d54c99695927872911a54c97c600
hccadmin <dave_rothfarb@dfci.harvard.edu>
2015-03-23 15:46:23 -0400

    Replaced HSPH logo with new one

--- templates/rt_afterburner/index.php
+++ templates/rt_afterburner/index.php
@@ -47,7 +47,11 @@ $is_home = JRequest::getVar('Itemid') == 101;
 
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://s</script>
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+	</script>
+
 </head>
 <body>
 <div id="header_left" class="absolute"></div>
